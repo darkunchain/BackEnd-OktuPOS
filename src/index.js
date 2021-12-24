d6f7bfb1642f09515f82bd74aeaa25d92172fce2
darkunchain <luisfer74081@gmail.com>
2021-12-23 22:39:57 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ const cors = require('cors');
 require('./db')
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT, GET, POST, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use('/api',require('./routes/userRoute'))
 app.listen(port, () => console.log(`Server on port ${port}!`))
